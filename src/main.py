8b8f7ed74e31c4729b237fe13518393e23d0fbce
DB8LE <db8le@darc.de>
2025-10-20 19:34:12 +0200

    Fix copy paste mistake

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from . import config, logging, notifier
 
 
 def main():
-    logging.set_up_logging("rsdb-map") # Set up logging
+    logging.set_up_logging("sonde-notifier") # Set up logging
 
     conf = config.read_config() # Read config
     logging.set_logging_config(conf) # Set logging config
